c1074be2b1f9c86d45127666ab623b1946bfe7d5
AJ <63694897+AadiCJ@users.noreply.github.com>
2022-11-06 22:50:04 +0530

    Added comments
    added comments and made some modifications to the Paint application.

--- Completed/Paint/Frame.java
+++ Completed/Paint/Frame.java
@@ -1,38 +1,44 @@
 import javax.swing.JFrame;
+/**
+ * the JFrame in which the entire paint application is held.
+*/
 public class Frame extends JFrame{
-	SidePanel sp = new SidePanel();
-	static Panel p = new Panel();
+	SidePanel sp = new SidePanel();//SidePanel extends JPanel
+	static Panel p = new Panel();//Panel extends JPanel.
 	public static void main(String[] args){
-		new Frame();
+		new Frame();//creates new frame.
 	}
 	Frame(){
 		setTitle("Paint");
-		setLayout(null);
-		setSize(750+sp.getWidth(), 750);
-		sp.setLocation(750, 0);
-		p.setLocation(0, 0);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLayout(null);//no layout manager, as absolute positioning was used.
+		setSize(750+sp.getWidth(), 750);//sp.getWidth() is used as the width needs to be accounted for 
+		sp.setLocation(750, 0);//location of the sidePanel.
+		p.setLocation(0, 0);//location of panel
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//EXIT_ON_CLOSE used, so that all JFrames open are closed.
 		add(p);
 		add(sp);
-		setLocationRelativeTo(null);
+		setLocationRelativeTo(null);//centering the frame/
 		setVisible(true);
 	}
-	static void setColor(int[] arr, String special){
-		if(arr[0] == -1 && special.equals("ColorSwitcher")){
+	static void setColor(int[] arr, String special){//setColor static method.
+		if(arr[0] == -1 && special.equals("ColorSwitcher")){//if color switcher is pressed,
+			//an array with -1 values and a String special = "ColorSwitcher" is passed in
 			openSwitcher();
-			return;
+			return;//method execution stops because otherwise p.setColor({-1, -1, -1}); would be called
+			//leading to an error.
 		}
-		else if(special.equals("Eraser")){
+		else if(arr[0] == -1 && special.equals("Eraser")){//same as colorswitcher, but for eraser
 			p.setColor(p.getBackground());
 			return;
 		}
 		p.setColor(arr);
 	}
 	static void openSwitcher(){
-		new ColorSwitcher();
+		new ColorSwitcher();//creates a new object.
 	}
 	static void setBrushSize(int in){
-		p.setBrushSize(in);
+		p.setBrushSize(in);//static methods are used so that other classes can call them, and the 
+		//JPanel panel can be affected.
 	}
 	static void fillBack(){
 		p.fillBack();
